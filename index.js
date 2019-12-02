83c31626bf14d3a81801b80d7a26c7968e63eae0
Georgiy Slobodenyuk <georgiy@absoluteweb.com>
2019-12-02 14:43:25 -0800

    feat(multistore): Add multistore capability

--- index.js
+++ index.js
@@ -3,8 +3,49 @@ import StoryblokClient from 'storyblok-js-client'
 import {apiStatus} from '../../../lib/util'
 
 module.exports = ({config, db}) => {
-  if (!config.storyblok || !config.storyblok.previewToken) {
+  const multistore = config.extensions.storyblok 
+    && config.extensions.storyblok.multistore 
+    && config.extensions.storyblok.config.length;
+  if (!multistore && (!config.storyblok || !config.storyblok.previewToken)) {
     throw new Error('🧱 : config.storyblok.previewToken not found')
+  }
+
+  const api = Router()
+
+  function loadStories(params) {
+    return storyblokClient.get('cdn/stories', Object.assign({
+      per_page: 100,
+      resolve_links: 'url'
+    }, params))
+  }
+
+  if (multistore) {
+    // map for multistore
+    config.extensions.storyblok.config.map(stConfig => {
+      if(!stConfig.previewToken) {
+        throw new Error('🧱 : config.extensions.storyblok[] item does not have a previewToken!')
+      }
+      const storyblokClientConfig = {
+        accessToken: stConfig.previewToken,
+        cache: {
+          type: 'memory'
+        }
+      }
+      const index = stConfig.indexName
+      const storyblokClient = new StoryblokClient(storyblokClientConfig)
+      const storeRouter = Router()
+      
+      storeRouter.get('/stories', async (req, res) => {
+        const path = req.params.story + req.params[0]
+        let dataJson = await loadStories(req.query)
+        apiStatus(res, {
+          stories: dataJson.data.stories,
+        })
+      })
+      api.use('/' + index, storeRouter)
+    });
+
+    return api;
   }
 
   const storyblokClientConfig = {
@@ -15,7 +56,6 @@ module.exports = ({config, db}) => {
   }
 
   const storyblokClient = new StoryblokClient(storyblokClientConfig)
-  const api = Router()
 
   api.get('/stories', async (req, res) => {
     const path = req.params.story + req.params[0]
@@ -25,13 +65,6 @@ module.exports = ({config, db}) => {
       stories: dataJson.data.stories,
     })
   })
-
-  function loadStories(params) {
-    return storyblokClient.get('cdn/stories', Object.assign({
-      per_page: 100,
-      resolve_links: 'url'
-    }, params))
-  }
 
   return api
 }
